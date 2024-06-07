d4b1c8246e78ea355e96100aef8f55b337b186a1
Erfan Hekmati <erfanhekmati8@gmail.com>
2024-06-07 12:36:50 +0330

    Update todo list dto updated

--- src/todos/domain/dtos/update-todo-list.dto.ts
+++ src/todos/domain/dtos/update-todo-list.dto.ts
@@ -1,7 +1,11 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class UpdateTodoListDto {
+  @ApiProperty()
   @IsNotEmpty()
-  @IsString()
+  @IsString({ message: 'Title should be string.' })
+  @MinLength(5, { message: 'Title should be at least 5 characters.' })
+  @MaxLength(100, { message: 'Title should be at most 100 characters.' })
   title: string;
 }
